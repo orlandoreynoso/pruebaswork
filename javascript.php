32568a870a09a1ba487728e1951c832a404d3b91
orlando reynoso job <orlandoreynoso01@gmail.com>
2018-01-31 12:13:28 -0600

    delete file modles

--- javascript.php
+++ javascript.php
@@ -16,6 +16,6 @@
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et alias, nulla odio soluta sunt pariatur ullam, tempore deleniti distinctio inventore recusandae fuga facere delectus veniam voluptatum nostrum dignissimos. Modi, laborum.</p>
 	</div>
 </div>
-<script src="js/curso2.js"></script>
+<script src="js/arreglos.js"></script>
 </body>
 </html>
---
+++ js/arreglos.js
@@ -0,0 +1,3 @@
+const numeros = [10,20,30,40,50,60];
+
+console.log(numeros.indexOf(50));
